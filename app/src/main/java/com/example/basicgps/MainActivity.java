618aa90951ce43a6debea141c4cf8b5780b032b9
chelsealau <chelseahlau@gmail.com>
2022-11-01 17:06:53 -0400

    Added updateGPS function body and updateUI

--- app/src/main/java/com/example/basicgps/MainActivity.java
+++ app/src/main/java/com/example/basicgps/MainActivity.java
@@ -1,22 +1,31 @@
+// REFERENCE: https://www.youtube.com/watch?v=_xUcYfbtfsI
+// Android Studio Tutorial - Build a GPS App
+
 package com.example.basicgps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+//import android.location.LocationRequest;
+import android.nfc.Tag;
 import android.os.Build;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Switch;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_FINE_LOCATION = 99;
@@ -36,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         tv_speed = findViewById(R.id.tv_speed);
         tv_sensor = findViewById(R.id.tv_sensor);
         tv_updates = findViewById(R.id.tv_updates);
-        tv_address = findViewById(R.id.tv_address);
+//        tv_address = findViewById(R.id.tv_address);
         sw_gps = findViewById(R.id.sw_gps);
         sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
 
@@ -45,6 +54,7 @@ public class MainActivity extends AppCompatActivity {
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(locationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
+//        locationRequest = LocationRequest.
 
         sw_gps.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -59,21 +69,73 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        updateGPS();
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case PERMISSIONS_FINE_LOCATION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    updateGPS();
+                } else {
+                    Toast.makeText(this, "This app requires permission to be granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
+
+    /**
+     * @brief Get permissions from user to track gps before getting location and updating UI
+     */
     private void updateGPS(){
+        // get permissions from user to track GPS
+        // get current location from fused client
+        // update UI with values
+
+        // get associated location provider client for main activity
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+        // checks if permission to access fine location is granted
         if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            // get location stored in memory
             fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
                 @Override
                 public void onSuccess(Location location) {
-
+                    // use location class to update text views
+                    if (location != null) {
+                        updateUIValues(location);
+                    } else
+                        Log.d("location", "location is NULL");
                 }
             });
         }
-        else{
+        else {
+
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_FINE_LOCATION);
+//                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSIONS_COURSE_LOCATION);
             }
         }
+    }
+
+    /**
+     * @brief Update text views with location values
+     */
+    private void updateUIValues(Location location) {
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+
+        if (location.hasSpeed()) {
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        } else {
+            tv_speed.setText("NOT AVAILABLE");
+        }
+
+
     }
 }

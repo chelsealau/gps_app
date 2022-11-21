5b902eda5077d4e96e2de1fde4fd151d9d6fe405
Arnaudh <arnaudh@bu.edu>
2022-11-20 21:10:47 -0500

    almost working email

--- app/src/main/java/com/example/basicgps/sendEmail.java
+++ app/src/main/java/com/example/basicgps/sendEmail.java
@@ -1,15 +1,20 @@
 package com.example.basicgps;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 public class sendEmail extends AppCompatActivity {
+    private String subject = "All Database Data";
+    private String body = "Find Database info in attatchment";
+
     AppCompatButton back_button, send_button;
     EditText email;
 
@@ -17,9 +22,29 @@ public class sendEmail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.send_email);
-        email = findViewById(R.id.email_addr);
         back_button = findViewById(R.id.back_button);
+        email = findViewById(R.id.email_addr);
         send_button = findViewById(R.id.send_button);
+        send_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!email.getText().toString().isEmpty()) {
+                    Intent intent = new Intent(Intent.ACTION_SEND);
+                    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{email.getText().toString()});
+                    intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+                    intent.putExtra(Intent.EXTRA_TEXT, body);
+//                    intent.setType("message/rfc822");
+                    intent.setData(Uri.parse("mailto:"));
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(sendEmail.this, "There is no application that support this action", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(sendEmail.this, "please fill all the fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,66 +55,4 @@ public class sendEmail extends AppCompatActivity {
         Intent main = getIntent();
     }
 }
-//    AppCompatButton back_button;
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.send_email);
-//
-//        back_button = findViewById(R.id.back_button);
-//
-//
-//        back_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(sendEmail.this, Group3.class);
-//                startActivity(intent);
-//            }
-//        });
-//        Intent main = getIntent();
-//    }
-//}
 
-
-
-//
-//public class getHighScore extends AppCompatActivity {
-//    AppCompatButton back_button;
-//    TextView max_speed, max_distance, max_time;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_get_high_score);
-//
-//        back_button = findViewById(R.id.back_button);
-//        max_distance = (TextView) findViewById(R.id.max_dist_val);
-//        max_time = (TextView) findViewById(R.id.max_time_val);
-//        max_speed = (TextView) findViewById(R.id.max_speed_val);
-//
-//        back_button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(getHighScore.this, Group3.class);
-//                startActivity(intent);
-//            }
-//        });
-//        Intent main = getIntent();
-//        String distance = main.getStringExtra("dist");
-//        String speed = main.getStringExtra("speed");
-//        String time = main.getStringExtra("time");
-//
-//        max_time.setText(time);
-//        max_speed.setText(speed);
-//        max_distance.setText(distance);
-//
-////        String speed = main.getStringExtra("speed");
-////        String time = main.getStringExtra("time");
-////        Toast.makeText(getHighScore.this, distance, Toast.LENGTH_LONG).show();
-////        if (Double.parseDouble(distance) > Double.parseDouble((String) max_distance.getText())){
-////            max_distance.setText(distance);
-////        }
-//
-////        Toast.makeText(getHighScore.this, "distance", Toast.LENGTH_LONG).show();
-//    }
-//}
